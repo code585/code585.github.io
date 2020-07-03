0008ce1fced8506d9b129246be1b7fdb74814ac3
unknown <dditmer@gmail.com>
2020-07-03 00:08:34 -0400

    add notinvited script and eulogies page

---
+++ notinvited.js
@@ -0,0 +1,25 @@
+$(() => {
+    let donate = false;
+    $("#donate").click(()=> {
+        donate = true;
+        $("#donate").html("<h3>Thanks for the support!</h3>");
+    });
+
+    $("input#name").keyup(() => {
+        if($("#name").val().trim().length > 0) {
+            $(".lettername").html($("#name").val());
+        } else {
+            $(".lettername").html($("#name")[0].placeholder);
+        }
+    });
+
+    $("#formsubmit").click(() => {
+        if (donate) {
+            $("#letter .card-header").html("Your letter is its way!");
+        } else {
+            $("#donate").hide().fadeIn();
+        }
+    })
+
+
+});
