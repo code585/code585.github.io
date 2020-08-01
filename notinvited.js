fbe675653a27dec27f0ac018331c020d10abfb92
unknown <dditmer@gmail.com>
2020-08-01 13:37:55 -0400

    change js loading order, add recaptcha last, move submitAPI outside of onload

--- notinvited.js
+++ notinvited.js
@@ -46,43 +46,6 @@ $(() => {
         }
     });
 
-    /*
-     * Callback for recaptcha, g-recaptcha
-     */
-    
-    function submitToAPI(token) {
-        const URL = "https://w64tgkz27g.execute-api.us-east-1.amazonaws.com/v1/emailhim";
-
-        let name = $("input#name").val();
-        let email = $("input#email").val();
-
-        let data = {
-           name : name,
-           email : email,
-           recaptcha: token
-         };
- 
-        $.ajax({
-          type: "POST",
-          url : URL,
-          dataType: "json",
-          crossDomain: true,
-          contentType: "application/json; charset=utf-8",
-          data: JSON.stringify(data),
- 
-          
-          success: () => {
-            // clear form and show a success message
-            console.log("Successful");
-            $("#letter .card-header").html("Your letter is its way!");
-          },
-          error: () => {
-            // show an error message
-            console.log("UnSuccessful");
-            $('#letter .card-header').html("Something went wrong (Maybe Russian Interference), The president did not get your letter.")
-          }});
-      }
-
     function validateEmail(email) {
         //From Angular
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -95,3 +58,41 @@ $(() => {
         return re.test(name);
     }
 });
+
+/*
+ * Callback for recaptcha, g-recaptcha
+ */
+
+function submitToAPI(token) {
+    const URL = "https://w64tgkz27g.execute-api.us-east-1.amazonaws.com/v1/emailhim";
+
+    let name = $("input#name").val();
+    let email = $("input#email").val();
+
+    let data = {
+        name : name,
+        email : email,
+        recaptcha: token
+        };
+
+    $.ajax({
+        type: "POST",
+        url : URL,
+        dataType: "json",
+        crossDomain: true,
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(data),
+
+        
+        success: () => {
+            // clear form and show a success message
+            console.log("Successful");
+            $("#letter .card-header").html("Your letter is its way!");
+        },
+        error: () => {
+            // show an error message
+            console.log("UnSuccessful");
+            $('#letter .card-header').html("Something went wrong (Maybe Russian Interference), The president did not get your letter.")
+        }
+    });
+}
